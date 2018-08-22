85a00b348a00f9f81e23e9631d8071c3704beeaf
Marcos Mellado <marcos@mellados.com>
2018-08-23 00:27:50 +0200

    Add back to top

--- src/components/App.vue
+++ src/components/App.vue
@@ -54,6 +54,9 @@ export default {
         <Menu v-bind:isMobileMenuOpen="isMobileMenuOpen" v-bind:toggleMobileMenu="toggleMobileMenu" v-bind:closeMenu="closeMenu"></Menu>
       </aside>
       <div v-html="gameMarkup"></div>
+      <a class="back-to-top" href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 284.929 284.929">
+        <path d="M282.082 195.285L149.028 62.24c-1.901-1.903-4.088-2.856-6.562-2.856s-4.665.953-6.567 2.856L2.856 195.285C.95 197.191 0 199.378 0 201.853c0 2.474.953 4.664 2.856 6.566l14.272 14.271c1.903 1.903 4.093 2.854 6.567 2.854s4.664-.951 6.567-2.854l112.204-112.202 112.208 112.209c1.902 1.903 4.093 2.848 6.563 2.848 2.478 0 4.668-.951 6.57-2.848l14.274-14.277c1.902-1.902 2.847-4.093 2.847-6.566.001-2.476-.944-4.666-2.846-6.569z"/>
+        </svg></a>
     </main>
     <footer>
       <p>
@@ -161,5 +164,24 @@ header h1.ultra-sun-moon {
 header h1.sun-moon {
   background: url('../img/sun-moon.png');
   background-size: cover;
+}
+
+.back-to-top {
+  width: 50px;
+  height: 50px;
+  background: rgba(102, 102, 102, 0.6);
+  color: #fff;
+  position: fixed;
+  bottom: 50px;
+  right: 0;
+  display: block;
+  text-align: center;
+}
+
+.back-to-top svg {
+  width: 35px;
+  margin-top: 5px;
+  height: 35px;
+  fill: #fff;
 }
 </style>
